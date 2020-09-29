3ba145b18a29a7dcfbbe5ad5cef7592b466a64f2
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-09-29 18:22:54 -0400

    frsh

---
+++ config/webpack/environment.js
@@ -0,0 +1,18 @@
+//  config/webpack/environment.js
+
+const { environment } = require('@rails/webpacker')
+
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+
+$: 'jquery',
+
+jQuery: 'jquery',
+
+Popper: ['popper.js', 'default']
+
+}))
+
+
+module.exports = environment
